0b8ad9a969b28d55ae092887f29ec2aa95268553
marcklinux <marcklinux@gmail.com>
2021-01-31 22:21:46 -0600

    pagina web de cliente en prueba

---
+++ js/menu.js
@@ -0,0 +1,16 @@
+
+
+var btnMenu = document.getElementById('btnmenu');
+
+var menu = document.getElementById('menu');
+
+
+btnMenu.addEventListener('click', function(){
+    'use strict';
+    
+    menu.classList.toggle('mostrar');
+    console.log("no encuentro el error");
+    
+    
+});
+
